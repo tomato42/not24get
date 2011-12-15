c02181863b605ebdb255fb56c6eb4051bc43c92b
Hubert Kario <kario@wit.edu.pl>
2011-12-15 22:51:11 +0100

    count number of words and proper quality checking

--- not24get.c
+++ not24get.c
@@ -24,12 +24,14 @@ int min_words = 3;
 // maximum length of password
 int max_len = 40;
 
-int read_config ()
+int
+read_config ()
 {
   return LDAP_SUCCESS;
 }
 
-int count_classes(char *password)
+int
+count_classes (char *password)
 {
   int lcase = 0;
   int ucase = 0;
@@ -41,89 +43,131 @@ int count_classes(char *password)
 
   while (*p != '\0')
     {
-      if (islower(*p))
-        lcase=1;
-      else if (isupper(*p))
-        ucase=1;
-      else if (isdigit(*p))
-        digits=1;
-      else if (ispunct(*p))
-        punct=1;
-      else if (!isspace(*p) && !iscntrl(*p))
-        other=1;
+      if (islower (*p))
+	lcase = 1;
+      else if (isupper (*p))
+	ucase = 1;
+      else if (isdigit (*p))
+	digits = 1;
+      else if (ispunct (*p))
+	punct = 1;
+      else if (!isspace (*p) && !iscntrl (*p))
+	other = 1;
       p++;
     }
 
   return lcase + ucase + digits + punct + other;
 }
 
-int check_password (char *pPasswd, char **ppErrStr, void *pEntry)
+// count number of "words" in password, where word is an alpha sequence of
+// at least 3 characters
+int
+count_words (char *password)
 {
-  if  (!pPasswd)
+  int words = 0;
+  int word_len = 0;
+  int is_word = 0;
+  char *p;
+
+  for (p = password; *p != '\0'; p++)
     {
-      *ppErrStr = strdup("Null password");
+      // word just started
+      if (isalpha (*p) && !is_word)
+	{
+	  is_word = 1;
+	  word_len = 1;
+	  continue;
+	}
+
+      // word just ended
+      if (!isalpha (*p) && is_word)
+	{
+	  is_word = 0;
+	  if (word_len >= 3)
+	    words++;
+	  word_len = 0;
+	  continue;
+	}
+
+      // word is in progress, count letters
+      if (isalpha (*p) && is_word)
+	{
+	  word_len++;
+	  continue;
+	}
+
+      // no word in progress, just continue
+    }
+
+  if (is_word && word_len >= 3)
+    words++;
+
+  return words;
+}
+
+int
+check_password (char *pPasswd, char **ppErrStr, void *pEntry)
+{
+  *ppErrStr = NULL;
+
+  if (!pPasswd)
+    {
+      *ppErrStr = strdup ("Null password");
       return FAILURE;
     }
 
-  if(read_config())
+  if (read_config ())
     {
-      *ppErrStr = strdup("Internal error");
+      *ppErrStr = strdup ("Internal error");
       return FAILURE;
     }
 
-  int pass_len = strnlen(pPasswd, MAX_LENGTH);
+  int pass_len = strnlen (pPasswd, MAX_LENGTH);
 
   if (pass_len == MAX_LENGTH || pass_len >= max_len)
     {
-      *ppErrStr = strdup("Password too long");
+      *ppErrStr = strdup ("Password too long");
       return FAILURE;
     }
 
-  if (pass_len <= 6 )
+  if (pass_len <= 6)
     {
-      *ppErrStr = strdup("Password REALLY short");
+      *ppErrStr = strdup ("Password REALLY short");
       return FAILURE;
     }
 
-  int pass_class = count_classes(pPasswd);
+  int pass_class = count_classes (pPasswd);
 
-  switch (pass_class)
+  if (pass_class == 0)
     {
-    case 0:
-      *ppErrStr = strdup("Password has no printable characters");
-      return FAILURE;
-    case 1:
-      if (min_1class == -1 || pass_len < min_1class)
-        {
-          *ppErrStr = strdup("Password too simple");
-          return FAILURE;
-        }
-      break;
-    case 2:
-      if (min_2class == -1 || pass_len < min_2class)
-        {
-          *ppErrStr = strdup("Password too simple");
-          return FAILURE;
-        }
-      break;
-    case 3:
-      if (min_3class == -1 || pass_len < min_3class)
-        {
-          *ppErrStr = strdup("Password too simple");
-          return FAILURE;
-        }
-      break;
-    case 4:
-      if (min_4class == -1 || pass_len < min_4class)
-        {
-          *ppErrStr = strdup("Password too simple");
-          return FAILURE;
-        }
-      break;
-    default:
-      *ppErrStr = strdup("Internal error");
+      *ppErrStr = strdup ("Password has no printable characters");
       return FAILURE;
     }
 
-  return 0;
+  if (pass_class >= 1)
+    if (min_1class != -1 && pass_len >= min_1class)
+      return LDAP_SUCCESS;
+  if (pass_class >= 2)
+    if (min_2class != -1 && pass_len >= min_2class)
+      return LDAP_SUCCESS;
+  if (pass_class >= 3)
+    if (min_3class != -1 && pass_len >= min_3class)
+      return LDAP_SUCCESS;
+  if (pass_class >= 4)
+    if (min_4class != -1 && pass_len >= min_4class)
+      return LDAP_SUCCESS;
+
+  if (pass_class < 2)
+    {
+      *ppErrStr = strdup ("Password too simple");
+      return FAILURE;
+    }
+
+  int pass_words = count_words (pPasswd);
+
+  if (min_words != -1 && pass_words >= min_words)
+    return LDAP_SUCCESS;
+
+  *ppErrStr = strdup ("Password too simple");
+  return FAILURE;
 }
